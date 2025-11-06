640adbc0b1c2be2fe2f5e0fd427fa4352e7c7dc2
Jeff Uchitjil <git@k9kld.org>
2025-11-06 16:15:44 -0600

    VIX-3750 Remove unused classes and commented code
    * Removed the IntervalUpdateSignaler and ControllerUpdateAdjucator as they are not longer needed.
    * IOutputDeviceUpdateSignaler should at least be deprecated and could be be removed.

--- src/Vixen.Core/Sys/Execution.cs
+++ src/Vixen.Core/Sys/Execution.cs
@@ -1,14 +1,8 @@
-﻿using System.Collections.Concurrent;
-using System.Diagnostics;
-using System.Threading;
-using Vixen.Execution;
-using Vixen.Execution.Context;
+﻿using System.Diagnostics;
 using Vixen.Instrumentation;
 using Vixen.Sys.Instrumentation;
 using Vixen.Sys.Managers;
-using Vixen.Sys.Output;
 using Vixen.Sys.State.Execution;
-using Vixen.Utility;
 
 namespace Vixen.Sys
 {
@@ -16,15 +10,11 @@ namespace Vixen.Sys
 	{
 		internal static SystemClock SystemTime = new SystemClock();
 		private static ExecutionStateEngine _state;
-		//private static ControllerUpdateAdjudicator _updateAdjudicator;
 		private static MillisecondsValue _executionUpdateTime;
 		private static MillisecondsValue _executionSleepTime;
-		private static MillisecondsValue _systemDeniedUpdateTime;
-		private static MillisecondsValue _systemDeniedBlockTime;
 		private static RateValue _executionUpdateRate;
 		private static Stopwatch _stopwatch;
-		private static long lastMs = 0;
-		private static bool lastUpdateClearedStates = false;
+		private static bool _lastUpdateClearedStates = false;
 		private static Thread _executionThread;
 		
 		/// <summary>
@@ -38,11 +28,7 @@ namespace Vixen.Sys
 			VixenSystem.Instrumentation.AddValue(_executionUpdateTime);
 			_executionSleepTime = new MillisecondsValue("Execution sleep time");
 			VixenSystem.Instrumentation.AddValue(_executionSleepTime);
-			//_systemDeniedUpdateTime = new MillisecondsValue("System denied update");
-			//VixenSystem.Instrumentation.AddValue(_systemDeniedUpdateTime);
-			//_systemDeniedBlockTime = new MillisecondsValue("System denied block");
-			//VixenSystem.Instrumentation.AddValue(_systemDeniedBlockTime);
-
+			
 			_executionUpdateRate = new ExecutionEngineRefreshRateValue();
 			VixenSystem.Instrumentation.AddValue(_executionUpdateRate);
 			_stopwatch = Stopwatch.StartNew();
@@ -94,15 +80,6 @@ namespace Vixen.Sys
 		{
 		}
 
-		//private static ControllerUpdateAdjudicator _UpdateAdjudicator
-		//{
-		//	get
-		//	{
-		//		//*** user-configurable threshold value
-		//		return _updateAdjudicator ?? (_updateAdjudicator = new ControllerUpdateAdjudicator(10));
-		//	}
-		//}
-
 		private static ExecutionStateEngine _State
 		{
 			get { return _state ?? (_state = new ExecutionStateEngine()); }
@@ -138,9 +115,6 @@ namespace Vixen.Sys
 			get { return CurrentExecutionTime.ToString("m\\:ss\\.fff"); }
 		}
 
-		private static ConcurrentDictionary<string, TimeSpan> lastSnapshots = new ConcurrentDictionary<string, TimeSpan>();
-		private static readonly Object LockObject = new Object();
-
 		private static void UpdateState()
 		{
 			if (_stopwatch == null)
@@ -154,44 +128,40 @@ namespace Vixen.Sys
 			{
 				_stopwatch!.Restart();
 				
-				lock (LockObject)
+				bool elementsAffected = VixenSystem.Contexts.Update();
+				if (elementsAffected)
 				{
-					bool elementsAffected = VixenSystem.Contexts.Update();
-					if (elementsAffected)
+					VixenSystem.Elements.Update();
+					_lastUpdateClearedStates = false;
+					if (VixenSystem.OutputControllers.Any(x => x.IsRunning))
 					{
-						VixenSystem.Elements.Update();
-						lastUpdateClearedStates = false;
-						if (VixenSystem.OutputControllers.Any(x => x.IsRunning))
-						{
-							//Only update the filter chain if we have a controller running
-							VixenSystem.Filters.Update();
-							updateOutputTask = UpdateOutputDevicesAsync();
-						}
-
-						updatePreviewTask = UpdatePreviewsAsync();
-					}
-					else if (!lastUpdateClearedStates)
-					{
-						//No need to sample all the contexts as we were just told there are no elements effected.
-						VixenSystem.Elements.ClearStates();
-						lastUpdateClearedStates = true;
-						if (VixenSystem.OutputControllers.Any(x => x.IsRunning))
-						{
-							//Only update the filter chain if we have a controller running
-							VixenSystem.Filters.Update();
-							updateOutputTask = UpdateOutputDevicesAsync();
-						}
-
-						updatePreviewTask = UpdatePreviewsAsync();
+						//Only update the filter chain if we have a controller running
+						VixenSystem.Filters.Update();
+						updateOutputTask = UpdateOutputDevicesAsync();
 					}
 
-					Task.WhenAll(updatePreviewTask, updateOutputTask).Wait();
+					updatePreviewTask = UpdatePreviewsAsync();
+				}
+				else if (!_lastUpdateClearedStates)
+				{
+					//No need to sample all the contexts as we were just told there are no elements effected.
+					VixenSystem.Elements.ClearStates();
+					_lastUpdateClearedStates = true;
+					if (VixenSystem.OutputControllers.Any(x => x.IsRunning))
+					{
+						//Only update the filter chain if we have a controller running
+						VixenSystem.Filters.Update();
+						updateOutputTask = UpdateOutputDevicesAsync();
+					}
 
-					
-					_executionUpdateTime.Set(_stopwatch.ElapsedMilliseconds);
-					_executionUpdateRate.Increment();
+					updatePreviewTask = UpdatePreviewsAsync();
 				}
 
+				Task.WhenAll(updatePreviewTask, updateOutputTask).Wait();
+
+				_executionUpdateTime.Set(_stopwatch.ElapsedMilliseconds);
+				_executionUpdateRate.Increment();
+				
 				var sleepStart = _stopwatch.ElapsedMilliseconds;
 				Sleep();
 				_executionSleepTime.Set(_stopwatch.ElapsedMilliseconds - sleepStart);
@@ -253,50 +223,5 @@ namespace Vixen.Sys
 
 			await Task.WhenAll(UpdatePreviewTasks);
 		}
-
-		//public static ConcurrentDictionary<string, TimeSpan> UpdateState( out bool allowed)
-		//{
-		//	if (_stopwatch == null)
-		//		InitInstrumentation();
-		//	long nowMs = _stopwatch.ElapsedMilliseconds;
-		//	lock (LockObject) {
-		//		long lockMs = _stopwatch.ElapsedMilliseconds - nowMs;
-		//		bool allowUpdate = _UpdateAdjudicator.PetitionForUpdate();
-		//		if (allowUpdate) {
-		//			bool elementsAffected = VixenSystem.Contexts.Update();
-		//			if (elementsAffected)
-		//			{
-		//				VixenSystem.Elements.Update();
-		//				lastUpdateClearedStates = false;
-		//				if (VixenSystem.OutputControllers.Any(x => x.IsRunning))
-		//				{
-		//					//Only update the filter chain if we have a controller running
-		//					VixenSystem.Filters.Update();
-		//				}
-		//			}
-		//			else if(!lastUpdateClearedStates)
-		//			{
-		//				//No need to sample all the contexts as we were just told there are no elements effected.
-		//				VixenSystem.Elements.ClearStates();
-		//				lastUpdateClearedStates = true;
-		//				if (VixenSystem.OutputControllers.Any(x => x.IsRunning))
-		//				{
-		//					//Only update the filter chain if we have a controller running
-		//					VixenSystem.Filters.Update();
-		//				}
-		//			}
-
-		//			_executionSleepTime.Set( lockMs);
-		//			_executionUpdateTime.Set(_stopwatch.ElapsedMilliseconds - nowMs - lockMs);
-		//		}
-		//		else {
-		//			_systemDeniedBlockTime.Set(lockMs);
-		//			_systemDeniedUpdateTime.Set(_stopwatch.ElapsedMilliseconds - nowMs - lockMs);
-		//		}
-		//		lastMs = nowMs;
-		//		allowed = allowUpdate;
-		//		return lastSnapshots;
-		//	}
-		//}
 	}
 }
